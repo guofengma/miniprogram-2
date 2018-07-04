cd0eb3955be3fb719bfd573cfc9fe5522a1c77c1
zhuweiyou <i@zhuweiyou.com>
2018-07-04 12:11:29 +0800

    修复 10000 错误

--- src/api/request.js
+++ src/api/request.js
@@ -6,7 +6,8 @@ export const request = async ({url, method = 'GET', data = {}, header = {}}) => 
   header['X-User-Token'] = store.state.token;
   const {data: res} = await wxp.request({url, method, data, header});
   if (res.code === 10000) {
-    return store.dispatch('goLogin');
+    store.dispatch('goLogin');
+    throw wxp.error('请重新扫码或者复制 token', res);
   }
   if (res.code !== 0) {
     throw wxp.error(res.message, res);
